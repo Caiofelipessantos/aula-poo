d4038812077ab34f01af295dbac10834d8baa63c
Caiofelipessantos <caiofelipessantos@outlook.com>
2022-03-10 23:18:22 -0300

    added a method to check if the user input is a short numeric value

--- src/App.java
+++ src/App.java
@@ -2,34 +2,44 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
-        String idade, doses;
-        String msgErro = "Por favor, digite um valor válido.";
+        String idade, doses, msgError = "Valor inválido.";
         short i, d;
 
-        do {
-            idade = JOptionPane.showInputDialog(null, "Informe a sua idade.");
+        idade = JOptionPane.showInputDialog(null, "Informe a sua idade.");
+        if (!isAShortValue(idade)) {
+            JOptionPane.showMessageDialog(null, msgError, "Erro", 0);
+            System.exit(1); //Para o programa
+        } else {
             i = Short.parseShort(idade);
             if (i < 0) {
-                JOptionPane.showMessageDialog(null, msgErro, "Erro", 2);
-            }
-        } while (i < 0);
-
-        if (i < 18) {
-            JOptionPane.showMessageDialog(null, "Você não tem idade para entrar na festa.", "Entrada não permitida", 0);
-        } else {
-            do {
-                doses = JOptionPane.showInputDialog(null, "Informe o número de doses de vacina tomadas.");
-                d = Short.parseShort(doses);
-                if (d < 0) {
-                    JOptionPane.showMessageDialog(null, msgErro, "Erro", 2);
-                }
-            } while (d < 0);        
-
-            if (d < 3) {
-                JOptionPane.showMessageDialog(null, "Você não está totalmente imnunizado.", "Entrada não permitida", 0);
+                JOptionPane.showMessageDialog(null, "Idade precisa ser um número positivo.", "Erro", 1);
+            } else if (i < 18) {
+                JOptionPane.showMessageDialog(null, "Você não tem idade para entrar na festa.", "Entrada não permitida", 0);
             } else {
-                JOptionPane.showMessageDialog(null, "Você pode participar da festa!", "Tudo OK!", 1);
+                doses = JOptionPane.showInputDialog(null, "Informe o número de doses de vacina tomadas.");
+                if (!isAShortValue(doses)) {
+                    JOptionPane.showMessageDialog(null, msgError, "Erro", 1);
+                } else {
+                    d = Short.parseShort(doses);
+                    if (d < 3) {
+                        JOptionPane.showMessageDialog(null, "Você não está totalmente imnunizado.", "Entrada não permitida", 0);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Você pode participar da festa!", "Tudo OK!", 1);
+                    }
+                }
             }
-        }   
+        }
+    }
+
+    public static boolean isAShortValue(String num) {
+        if (num == null) {
+            return false;
+        } 
+        try {
+            short s = Short.parseShort(num);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
